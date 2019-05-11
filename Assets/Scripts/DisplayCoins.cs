4749848e133e15f52ce361915f65aa67403edafc
Ellpeck <megamaximal@gmail.com>
2019-05-11 20:08:24 +0200

    Added coin display

---
+++ Assets/Scripts/DisplayCoins.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayCoins : MonoBehaviour {
+
+    public PlayerController player;
+    private TextMeshProUGUI text;
+
+    private void Start() {
+        this.text = this.GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update() {
+        this.text.text = this.player.coinAmount.ToString();
+    }
+
+}
