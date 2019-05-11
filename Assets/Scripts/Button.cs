8f3989e9f245f4ec697a62101e4d6702ccb12593
Ellpeck <megamaximal@gmail.com>
2019-05-11 16:37:54 +0200

    added buttons that turn on invisible blocks

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    public Sprite upSprite;
+    public Sprite downSprite;
+    public float downSeconds;
+
+    private new SpriteRenderer renderer;
+    private bool isDown;
+
+    private InvisiBox[] invisiBoxesInScene;
+
+    private void Start() {
+        this.renderer = this.GetComponent<SpriteRenderer>();
+        this.invisiBoxesInScene = FindObjectsOfType<InvisiBox>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (!this.isDown && other.CompareTag("Player")) {
+            this.StartCoroutine(this.Press());
+        }
+    }
+
+    private IEnumerator Press() {
+        this.isDown = true;
+        this.renderer.sprite = this.downSprite;
+        foreach (var box in this.invisiBoxesInScene)
+            box.Enable();
+
+        yield return new WaitForSeconds(this.downSeconds);
+
+        this.isDown = false;
+        this.renderer.sprite = this.upSprite;
+        foreach (var box in this.invisiBoxesInScene)
+            box.Disable();
+    }
+
+}
---
+++ Assets/Scripts/InvisiBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisiBox : MonoBehaviour {
+
+    private new SpriteRenderer renderer;
+    private new BoxCollider2D collider;
+
+    private void Start() {
+        this.renderer = this.GetComponent<SpriteRenderer>();
+        this.collider = this.GetComponent<BoxCollider2D>();
+    }
+
+    public void Enable() {
+        this.renderer.enabled = true;
+        this.collider.enabled = true;
+    }
+
+    public void Disable() {
+        this.renderer.enabled = false;
+        this.collider.enabled = false;
+    }
+
+}
